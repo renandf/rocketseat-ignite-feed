8e4a808dfac2f77e080419e2f008013861cf6f92
Renan Castro <renandf@gmail.com>
2022-07-27 17:59:14 +1000

    feat: Create Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 import { Post} from "./Post"
+
+import styles from "./App.module.css"
 
 import "./global.css"
 
@@ -8,14 +11,20 @@ export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Renan Castro"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem ducimus velit explicabo. Necessitatibus nulla dolorum ex facere modi sit, provident repellat magnam aperiam excepturi hic ipsam ad assumenda et similique!"
-      />
-      <Post
-        author="Diego Fernandes"
-        content="Esse é um outro post."
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Renan Castro"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem ducimus velit explicabo. Necessitatibus nulla dolorum ex facere modi sit, provident repellat magnam aperiam excepturi hic ipsam ad assumenda et similique!"
+          />
+          <Post
+            author="Diego Fernandes"
+            content="Esse é um outro post."
+          />
+        </main>
+      </div>
     </>
   )
 }
