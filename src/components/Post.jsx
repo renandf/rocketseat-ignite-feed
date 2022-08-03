f12455fb541fb1584a504f42dfa59e51b1672df6
Renan Castro <renandf@gmail.com>
2022-08-03 21:20:00 +1000

    feat: Convert Avatar into its own component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar"
 import { Comment } from "./Comment"
 import styles from "./Post.module.css"
 
@@ -6,7 +7,7 @@ export function Post(props) {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/renandf.png" />
+          <Avatar src="https://github.com/diego3g.png" />
           <div className={styles.authorInfo}>
             <strong>Renan Castro</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react"
+import { Avatar } from "./Avatar"
 
 import styles from "./Sidebar.module.css"
 
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/renandf.png" />
+        <Avatar src="https://github.com/renandf.png" />
 
         <strong>User Name</strong>
         <span>Job Title</span>
