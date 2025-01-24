7a9ce6d5527bd8d3518fbcfb92f38b0ea08b3fa3
Renan Castro <renandf@gmail.com>
2025-01-24 18:42:54 +1000

    feat: Add the ability to delete a comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from "phosphor-react"
 import { Avatar } from "./Avatar"
 import styles from "./Comment.module.css"
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/renandf.png" />
@@ -19,7 +23,7 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Delete comment">
+            <button onClick={handleDeleteComment} title="Delete comment">
               <Trash size={24} />
             </button>
           </header>
